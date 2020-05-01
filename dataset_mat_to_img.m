9f02b052cd8e82dfa983f22a26fcdbd216093c81
lmtfm19 <lmavropalias@uoc.edu>
2020-05-01 08:37:43 +0200

    Update readme and add .mat to image covnerter

---
+++ dataset_mat_to_img.m
@@ -0,0 +1,13 @@
+% change inputFolder and outputFolder if necessary
+% so that they point to the directory containing .mat files
+inputFolder = 'D:\where\dataset\is\stored';
+outputFolder = pwd; % Please change, if needed.
+fileList = dir(fullfile(inputFolder,'*.mat'));
+
+for kk = 1:numel(fileList)
+  S = load(fullfile(pwd,fileList(kk).name));
+  I = S.cjdata.image;
+  I = mat2gray(I);
+  fileName = strrep(fileList(kk).name,'.mat','.tiff');
+  imwrite(I,fullfile(outputFolder,fileName));
+end
