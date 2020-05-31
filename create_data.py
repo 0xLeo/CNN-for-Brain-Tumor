3699f2d977b94983b842f2d898a442903ffb6449
lmtfm19 <lmavropalias@uoc.edu>
2020-05-31 04:58:31 +0200

    Remove scikit io dependancy, replace with opencv, add exceptions

--- create_data.py
+++ create_data.py
@@ -1,9 +1,6 @@
-#!/usr/bin/env python
 ## Author: Narmada M. Balasooriya   ##
 ##         University of Peradeniya ##
 ##         Sri Lanka                ##
-## Editor: Leontios Mavropalias     ##
-##         UoC                      ##
 
 #####################################
 ## Import the necessary libraries ###
@@ -11,13 +8,10 @@
 
 from __future__ import division, print_function, absolute_import
 
-from skimage import color, io
-from scipy.misc import imresize
 import numpy as np
 import os
 from glob import glob
 import cv2
-
 from six.moves import cPickle
 import pickle
 
@@ -27,17 +21,18 @@ np.set_printoptions(suppress=True)
 ### Imports picture files
 ########################################
 
-# TumorA = 0
-# TumorB = 1
-# TumorC = 2
-# TumorD = 3
-# TumorE = 4
+# TumorA = 
+# TumorB = 
+# TumorC = 
+# TumorD = 
+# TumorE = 
 
-files_path_tumorA = '/mnt/share/mri_dataset/cheng/non_seg/1'
-files_path_tumorB = '/mnt/share/mri_dataset/cheng/non_seg/2'
-files_path_tumorC = '/mnt/share/mri_dataset/cheng/non_seg/3'
-files_path_healthy = '/mnt/share/mri_dataset/cheng/non_seg/4'
-files_path_tumor_unknown = '/mnt/share/mri_dataset/cheng/non_seg/5'
+# TODO: rename: these are actually 5 tumour types
+files_path_tumorA = '/mnt/share/mri_dataset/cheng/full/1'
+files_path_tumorB = '/mnt/share/mri_dataset/cheng/full/2'
+files_path_tumorC = '/mnt/share/mri_dataset/cheng/full/3'
+files_path_healthy = '/mnt/share/mri_dataset/cheng/full/4'
+files_path_tumor_unknown = '/mnt/share/mri_dataset/cheng/full/5'
 
 tumorA_path = os.path.join(files_path_tumorA, '*.tiff')
 tumorB_path = os.path.join(files_path_tumorB, '*.tiff')
@@ -47,7 +42,6 @@ tumor_unknown_path = os.path.join(files_path_tumor_unknown, '*.tiff')
 
 print("tumor A path")
 
-# BUG: only tumor A is fuled, the rest empty
 tumorA = sorted(glob(tumorA_path))
 tumorB = sorted(glob(tumorB_path))
 tumorC = sorted(glob(tumorC_path))
@@ -67,70 +61,68 @@ count = 0
 y_count = 0
 for f in tumorA:
     try:
-        #img = io.imread(f)
         img = cv2.imread(f)
-        new_img = imresize(img, (size_image, size_image, 3))
+        new_img = cv2.resize(img,(size_image,size_image))
         allX[count] = np.array(new_img)
         ally[y_count] = 0
         count += 1
         y_count += 1
     except:
-        continue
+        raise SystemError("Uh-oh, something wrong with label A...")
 
 print("tumorA done")
 for f in tumorB:
     try:
-        #img = io.imread(f)
         img = cv2.imread(f)
-        new_img = imresize(img, (size_image, size_image, 3))
+        new_img = cv2.resize(img,(size_image,size_image))
         allX[count] = np.array(new_img)
         ally[y_count] = 1
         count += 1
         y_count += 1
     except:
-        continue
+        raise SystemError("Uh-oh, something wrong with label B...")
+
 print("tumorB done")
 for f in tumorC:
     try:
-        #img = io.imread(f)
         img = cv2.imread(f)
-        new_img = imresize(img, (size_image, size_image, 3))
+        new_img = cv2.resize(img,(size_image,size_image))
         allX[count] = np.array(new_img)
         ally[y_count] = 2
         count += 1
         y_count += 1
     except:
-        continue
+        raise SystemError("Uh-oh, something wrong with label C...")
+
 print("tumorC done")
 for f in no_tumor:
     try:
-        #img = io.imread(f)
         img = cv2.imread(f)
-        new_img = imresize(img, (size_image, size_image, 3))
+        new_img = cv2.resize(img,(size_image,size_image))
         allX[count] = np.array(new_img)
         ally[y_count] = 3
         count += 1
         y_count += 1
     except:
-        continue
-print("tumorD done")
+        raise SystemError("Uh-oh, something wrong with label D...")
+
+print("no tumor done")
 for f in tumor_unknown:
     try:
-        #img = io.imread(f)
         img = cv2.imread(f)
-        new_img = imresize(img, (size_image, size_image, 3))
+        new_img = cv2.resize(img,(size_image,size_image))
         allX[count] = np.array(new_img)
         ally[y_count] = 4
         count += 1
         y_count += 1
     except:
-        continue
-print("tumorE done")
+        raise SystemError("Uh-oh, something wrong with label E...")
+
+print("unknown done")
 print("images are arrayed")
 
-print("data are split")
 
-f = open('dataset_cheng_non_seg.pkl', 'wb')
+f = open('full_dataset_final.pkl', 'wb')
 
 print("pickle file open")
 cPickle.dump((allX, ally), f, protocol=cPickle.HIGHEST_PROTOCOL)
